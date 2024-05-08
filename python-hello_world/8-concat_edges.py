8d1eef70e40bc5aeab5f63ae049298473038b423
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-08 01:15:15 -0500

    delete back line

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-text = " with Python"
-print(str[39:66] + text)
+
+print(f"{str[39:66]} with Python")
