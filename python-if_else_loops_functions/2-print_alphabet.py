52166f299d11c76d9d96ff3487b257cdfa6d3019
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-10 23:07:22 -0500

    put end data

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # start and end ----section char imprimibles
 star = 97
-end = 122
+end = 123
 # using function char
 for n in range(star, end):
     print("{}".format(chr(n)), end="")
