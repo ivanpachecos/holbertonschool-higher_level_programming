d31de3732e8921df0f58ce701111b3dc12e1f000
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-11 16:48:44 -0500

    file islower function

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# assci table
+
+def islower(c):
+    start_lower = 97
+    end_lower = 123
+    for n in range(start_lower, end_lower):
+        if n == ord(c):
+            return True
+        else:
+            return False
