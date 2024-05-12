050526327daf2b4e24919728ac49818fdfc99e51
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-12 13:15:50 -0500

    uppercase function

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    text_upper = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            text_upper += chr(ord(char) - 32)
+        else:
+            text_upper += char
+    print(text_upper)
