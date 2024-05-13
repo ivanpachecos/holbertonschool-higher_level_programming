3fcf1e771f74db06f1c3a8136f5d8d492017b2a4
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-13 16:39:33 -0500

    print list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-        for target_list in my_list:
-            print("{}".format(target_list))
+    
+    for i in my_list:
+        print("{:d}".format(i))
