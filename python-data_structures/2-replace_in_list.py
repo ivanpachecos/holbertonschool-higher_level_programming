8a0109c1a66a7d49361042ce078295a55d0939f1
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-14 22:38:21 -0500

    replace in list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     len_list = len(my_list)
-    if idx < 0 or idx > len_list:
+    if idx < 0 or idx > len_list - 1:
         return my_list
     else:
         my_list[idx] = element
