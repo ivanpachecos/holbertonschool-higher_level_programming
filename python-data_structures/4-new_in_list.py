671dbcf89958d9798b750ceb39d8ad2eca501532
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-15 02:14:36 -0500

    new in list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     copy_list = my_list[:]
-    if idx < 0 or len(my_list) - 1:
-        return copy_list
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list[:]
     copy_list[idx] = element
     return copy_list
