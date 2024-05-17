2f69ef776d569825074e20b292e5938eafc26816
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-17 15:31:01 -0500

    TO remove an element from a list

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    copy_list = my_list[:]
+    if idx < 0 or idx > len(my_list) - 1:
+        return copy_list
+    else:
+        del my_list[idx]    
+        del copy_list[idx]    
+        return copy_list
