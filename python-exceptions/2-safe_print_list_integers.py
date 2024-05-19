4d2a567ccd90acf68d9b7684d0f77e489a732e0e
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-19 15:17:51 -0500

    save print list integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print_number += 1
         except (ValueError, TypeError):
             continue
+        except IndexError:
+            raise
     print()
     return print_number
