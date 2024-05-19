2e5a687f31fd3151b64c4436d8771de39fcbca70
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-19 16:08:35 -0500

    safe print division

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    finally:
+        print("{}".format(res))
+        return res
