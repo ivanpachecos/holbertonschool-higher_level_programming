695a28cf136db89dca0807fd3068a40cc6a2d68d
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-19 20:38:38 -0500

    Technical interview preparation:

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+
+    roman_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
     r_list = []
     rez = 0
+
     for c in roman_string:
-        r_list.append(roman_n.get(c))
+        if c in roman_n:
+            r_list.append(roman_n[c])
+        else:
+            return 0  # Si hay un carácter no válido en la cadena, retorna 0
 
     list_len = len(r_list)
     for i in range(list_len):
