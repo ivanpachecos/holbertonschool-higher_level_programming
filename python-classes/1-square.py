cfeed5a958faeea8c4cc682156b103676c2dcec4
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-19 22:19:17 -0500

    class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" square class """
+
+
+class Square():
+    """square"""
+
+    """
+        Private instance attribute: size
+    """
+    def __init__(self, size) -> None:
+        self.__prv_attribute = size
