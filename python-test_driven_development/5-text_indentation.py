ebd61e7cda351123d5824793a412ae63236b6b92
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-22 23:02:36 -0500

    function that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This script contains a function to insert a newline followed by two spaces after
+each occurrence of '.', '?', or ':' in the given text.
+"""
+
+
+def text_indentation(text):
+    """
+    Inserts a newline followed by two spaces after each occurrence
+    of '.', '?', or ':' in the given text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        None. The function prints the modified text with the specified indentation.
+    """
+
+    if not isinstance(text, (str)):
+        raise TypeError("text must be a string")
+
+    tex1 = text.replace(".", ".\n \n")
+    tex2 = tex1.replace("?", "?\n \n")
+    tex3 = tex2.replace(":", ":\n \n")
+
+    # Print the modified text
+    print(tex3)
