4033b18ef13092e11d3a2a96a9c9e56487a787ec
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-25 01:10:52 -0500

    Since the beginning you have been creating “Interactive tests”. For this exercise, you will add Unittests.

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -17,7 +17,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertIsNone(max_integer([]))
 
     def test_max_at_the_end(self):
-        self.assertEqual([1, 2],2)
+        self.assertEqual(max_integer([1, 2, 4, 5]), 5)
 
 if __name__ == '__main__':
     unittest.main()
