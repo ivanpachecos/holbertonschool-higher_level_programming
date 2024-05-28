0f947185f0b2364bc1f0aedf0c442ef99af76fd7
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-27 20:19:39 -0500

    read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Function that read text of other file
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads the content of a file and prints it to the console.
+    """
+    with open(filename, 'r') as f:
+        text_readed = f.read()
+        print(text_readed)
