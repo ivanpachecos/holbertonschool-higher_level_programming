4cfcfc2055696d51707b5a25818f864e5916defe
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-27 20:27:04 -0500

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     """
     with open(filename, 'r') as f:
         text_readed = f.read()
-        print(text_readed)
+        print("{}".format(text_readed), end="")
