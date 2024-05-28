ca24d4c06b07d742d1f87b6e592cad06645c1bfa
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-28 11:48:36 -0500

    Appends a given text to the end of a specified file.

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,13 +8,6 @@ number of characters added.
 def append_write(filename="", text=""):
     """
     Appends a given text to the end of a specified file.
-
-    Args:
-        filename (str): The name of the file to which the text will be appended. Defaults to an empty string.
-        text (str): The text to append to the file. Defaults to an empty string.
-
-    Returns:
-        int: The number of characters written to the file.
     
     Example:
         >>> append_write("example.txt", "Hello, World!")
