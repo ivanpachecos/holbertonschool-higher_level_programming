1cbc3bad999d047fef5e48ada41dcd74f87abc9d
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-29 02:38:24 -0500

    Function thar returns true if the object is an instace

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Write a function that returns True if the object is an
+instance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ return the instance if is true """
+    return isinstance(obj, a_class)
