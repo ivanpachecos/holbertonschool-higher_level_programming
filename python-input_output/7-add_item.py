eef853ac2e68cd97968332427a912082e406dc5c
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-30 18:49:30 -0500

    script that adds all arguments to a Python list, and then save them to a file:

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
+"""
+function that returns the dictionary description with simple data
+structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object.
+"""
+
 import json
 import sys
 import os
 
 
 def load_from_json_file(filename):
+    """Reads JSON data from a file and returns the corresponding
+    Python object."""
     with open(filename, 'r') as file:
         return json.load(file)
 
 def save_to_json_file(data, filename):
+    """Writes a Python object to a file in JSON format."""
     with open(filename, 'w') as file:
         json.dump(data, file)
 
 def main():
+    """Main function that loads, updates, and saves a
+    list to a JSON file."""
+
     filename = 'add_item.json'
 
     # comprobar si existe
