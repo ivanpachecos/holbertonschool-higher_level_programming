34ef85e3781652c0092e380968b52ebd04146a20
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-30 23:25:39 -0500

    Definig a Student class

---
+++ python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is a module for defining a Student class.
+"""
+
+
+class Student:
+    """A class representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the student with first name, last name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Return dict for students """
+        return {
+            'first_name' : self.first_name,
+            'last_name' : self.last_name,
+            'age' : self.age
+        }
