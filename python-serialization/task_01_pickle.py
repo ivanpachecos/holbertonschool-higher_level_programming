d2dec19c16df00fe6ecc25e4b1c41940e3835063
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-31 14:51:58 -0500

    This script defines class can be serialized and deserialized

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-"""Serialize and deserialize custom Python
-objects using the pickle module.
 """
+This script defines a `CustomObject` class that can be serialized and
+deserialized using Python's `pickle` module.
+The class includes methods to display the object's information
+serialize the object to a file, and deserialize the object from a file.
+"""
+
 import pickle
 
 
 class CustomObject:
     def __init__(self, name, age, is_student):
+        """Initializes an instance of the CustomObject class"""
         self.name = name
         self.age = age
         self.is_student = is_student
 
+    def display(self):
+        """Displays the object's information."""
+        print("Nombre: {}\nAge: {}\nIs student: {}"
+                .format(self.name, self.age, self.is_student))
+    
     def serialize(self, filename):
-        try:
-            with open(filename, 'wb') as file:
-                pickle.dump(self, file)
-        except (IOError, pickle.PickleError) as e:
-            print(f"Error serializing object: {e}")
-
+        """Serializes the object and saves it to a file."""
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+    
     @classmethod
     def deserialize(cls, filename):
-        try:
-            with open(filename, 'rb') as file:
-                return (pickle.load(file))
-        except (IOError, pickle.PickleError) as e:
-            print(f"Error deserializing object: {e}")
-            return (None)
-
-    def display(self):
-        print("Name: {}\nAge: {}\nIs Student: {}". format(
-            self.name, self.age, self.is_student))
+        """Deserializes an object from a file."""
+        with open(filename, 'rb') as file:
+            loaded_file = pickle.load(file)
+            return loaded_file
