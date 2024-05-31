7124b5897ad0f7e261c96e12f1e2464fd90b1bed
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-30 21:53:25 -0500

    convert to a json format

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,10 +4,7 @@ function that returns the dictionary description with simple
 data structure (list, dictionary, string, integer and boolean).
 """
 
-import json
-
 
 def class_to_json(obj):
-    """ Converts a Python object into a JSON format. """
-    json_conver = json.dumps(obj.__dict__)
-    return json_conver
+    """ Return dic using dict"""
+    return obj.__dict__
