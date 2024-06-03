4572cd02e0256fa11860025e2f122b5f13be77d4
ivanpachecos <ivanpachecos@yahoo.com>
2024-06-02 23:33:29 -0500

    Public instance method that raises an exception for area that is         not implemented

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
-"""Class BaseGeometry"""
+"""
+Class BaseGeometry
+"""
 
 
 class BaseGeometry:
+    """
+    A class used to represent the base geometry
+    """
+
     def area(self):
         """
         Public instance method that raises an exception for area that is
-        not implemented
-        """
+        not implemented.
 
+        Raises:
+            Exception: Indicates that the method is not implemented.
+        """
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
         """
-        Public instance method that validates value
+        Public instance method that validates the value as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('{} must be an integer'.format(name))
 
         if value <= 0:
