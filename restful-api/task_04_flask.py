ada11c8102c230d4ea37f272664280185daed8b2
ivanpachecos <ivanpachecos@yahoo.com>
2024-06-04 07:54:00 -0500

    Develop a Simple API using Python with Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,16 +1,22 @@
-#!/usr/bin/python3
 """
 This script sets up a simple HTTP server using Flask.
 The server handles various endpoints and serves text or JSON responses.
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+users = {
+    "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
+    }
 @app.route('/')
 def home():
     return 'Welcome to the Flask API!'
 
+@app.route('/data')
+def handle_data():
+    return jsonify(users)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
