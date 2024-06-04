caa8f3e754fbaf103bde9bfa0012c937be85d6fd
ivanpachecos <ivanpachecos@yahoo.com>
2024-06-03 22:59:49 -0500

    Develop a Simple API using Python with Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -9,8 +9,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home():
+    return 'Welcome to the Flask API!'
 
 if __name__ == '__main__':
     app.run(debug=True)
