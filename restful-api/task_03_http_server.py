78fb24351758bc1009a4bf790166eeaf8ff21c96
ivanpachecos <ivanpachecos@yahoo.com>
2024-06-03 19:06:12 -0500

    Develop a simple API using Python with the `http.server` module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""This script sets up a simple HTTP server using the http.server module.
+"""
+This script sets up a simple HTTP server using the http.server module.
 The server handles various endpoints and serves text or JSON responses.
 """
 
+# Importación de módulos necesarios
 import http.server
 import socketserver
 import json
 
-# Define the port on which the server will listen
 PORT = 8000
 
-class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-    """Request handler class to handle HTTP GET requests."""
 
+class SimpleRequest(http.server.BaseHTTPRequestHandler):
+    # Manejo de peticiones GET
     def do_GET(self):
-        """Handle GET requests for different endpoints."""
         if self.path == '/':
             self.handle_root()
         elif self.path == '/data':
@@ -25,15 +25,12 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.handle_not_found()
 
     def handle_root(self):
-        """Handle the root endpoint '/' by returning a simple text response."""
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        response = "Hello, this is a simple API!"
-        self.wfile.write(response.encode())
+        self.wfile.write("Hello, this is a simple API".encode())
 
     def handle_data(self):
-        """Handle the '/data' endpoint by returning a sample JSON dataset."""
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
@@ -49,13 +46,12 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write(response.encode())
 
     def handle_not_found(self):
-        """Handle undefined endpoints by returning a 404 Not Found response."""
         self.send_response(404)
-        self.send_header("Content-type", "application/json")
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
-        response = {"error": "Not Found", "message": "Endpoint not found"}
-        self.wfile.write(json.dumps(response).encode())
+        self.wfile.write("404 Not Found".encode())
 
-with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
-    print("Serving at port", PORT)
+# Configuración del servidor HTTP
+with socketserver.TCPServer(("", PORT), SimpleRequest) as httpd:
+    print("serving at port", PORT)
     httpd.serve_forever()
