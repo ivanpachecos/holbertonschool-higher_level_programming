58d27a16f035e94945f40c228270b1f4bfba79ac
ivanpachecos <ivanpachecos@yahoo.com>
2024-06-04 22:48:05 -0500

    task4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -21,26 +21,15 @@ def data():
     # Return the usernames as a JSON response
     return jsonify(usernames), 200
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    user_data = request.get_json()
-    username = user_data.get('username')
-    if not username:
-        return jsonify({"error": "Username is required"}), 400
-    if username in users:
-        return jsonify({"error": "User already exists"}), 400
-    users[username] = {
-        "name": user_data.get('name'),
-        "age": user_data.get('age'),
-        "city": user_data.get('city')
-    }
-    return jsonify({"message": "User added successfully"}), 201
+@app.route('/status')
+def status():
+    return 'OK'
 
-@app.route('/users/<username>', methods=['GET'])
+@app.route('/users/<username>')
 def get_user(username):
     if username not in users:
         return jsonify({"error": "User not found"}), 404
-    return jsonify(users[username]), 200
+    return jsonify(users), 200
 
 if __name__ == '__main__':
     app.run()
