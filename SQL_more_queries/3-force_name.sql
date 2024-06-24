e06f281a1e88535f67c19ed76e53662e826779cc
ivanpachecos <ivanpachecos@yahoo.com>
2024-06-23 20:28:03 -0500

    create new table

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- create tale --
 
-CREATE TABLE IF NOT EXISTS force_name (
+CREATE TABLE IF NOT EXISTS force_name(
     id INT,
-    name VARCHAR(256) NOT NULL;
-)
+    name VARCHAR(256) NOT NULL
+);
