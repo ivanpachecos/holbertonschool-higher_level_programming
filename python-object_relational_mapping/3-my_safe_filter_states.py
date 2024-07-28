29a6727b68756504bc0a8e4a2d43607a05e97b08
ivanpachecos <ivanpachecos@yahoo.com>
2024-07-28 16:39:18 -0500

    task3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,27 @@
-x#!/usr/bin/python3
+#!/usr/bin/python3
 """
-lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa, safe from injection
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+but safe from MySQL injections!
 """
-
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+# The code should not be executed when imported
+if __name__ == '__main__':
+
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name=%s\
-                    ORDER BY states.id ASC", (argv[4],))
-    rows = cursor.fetchall()
-    for state in rows:
-        print(state)
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
